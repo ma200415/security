fcdc085f9055525c00a4d5ae94dd9c5c5f05f15e
ma200415 <ma20200415@gmail.com>
2022-09-09 23:03:04 +0800

    fix cannot run by cli

--- reminderemail.php
+++ reminderemail.php
@@ -1,30 +1,31 @@
 <?php
-include_once 'navbar.php';
-redirectHomeIfNotLoggedIn(["admin"]);
+if (isset($_SERVER["PHP_SELF"]) && $_SERVER["PHP_SELF"] == 'C:\xampp\htdocs\reminderemail.php') {
+    include_once 'navbar.php';
 
-try {
-    $dbh = pdo();
-    $sql = 'SELECT id, (select email from user where id=user) email, redemptionPlace, reservationDate, reservationTime 
-            FROM booking 
-            WHERE DATEDIFF(reservationDate, CURRENT_DATE) <= 2 
-                AND status = "approve" 
-                AND reminderSent IS NOT TRUE';
+    try {
+        $dbh = pdo();
+        $sql = 'SELECT id, (select email from user where id=user) email, redemptionPlace, reservationDate, reservationTime 
+                FROM booking 
+                WHERE DATEDIFF(reservationDate, CURRENT_DATE) <= 2 
+                    AND status = "approve" 
+                    AND reminderSent IS NOT TRUE';
 
-    $sth = $dbh->prepare($sql);
-    $sth->execute();
-    $remindBookings = $sth->fetchAll();
-} catch (PDOException $e) {
-    echo $e->getMessage();
-    exit;
-}
-
-try {
-    foreach ($remindBookings as $key => $reminder) {
-        sendReminderEmail(
-            $reminder,
-            "ID Card Appointment Reminder"
-        );
+        $sth = $dbh->prepare($sql);
+        $sth->execute();
+        $remindBookings = $sth->fetchAll();
+    } catch (PDOException $e) {
+        echo $e->getMessage();
+        exit;
     }
-} catch (Exception $e) {
-    echo $e->getMessage();
+
+    try {
+        foreach ($remindBookings as $key => $reminder) {
+            sendReminderEmail(
+                $reminder,
+                "ID Card Appointment Reminder"
+            );
+        }
+    } catch (Exception $e) {
+        echo $e->getMessage();
+    }
 }
